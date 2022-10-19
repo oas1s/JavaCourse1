1b0f8f7504ffbc15070323708a5a2997a52465ff
oas1s <azatazat835@mail.ru>
2022-10-19 17:20:15 +0300

    add classwork

--- kr_correct/Main.java
+++ kr_correct/Main.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
+/*
+Вариант 1 задание 3.
+ */
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        double x = scanner.nextDouble();
+        double sum = 0;
+        for (int i = 1; i <=n; i++) {
+            int factt = fact(i);
+            double x1 = Math.pow(-1,i);
+            double x2 = Math.pow(x,2*i+1);
+            sum += (x1*x2)/(factt + x);
+        }
+        System.out.println(sum);
+    }
+
+    public static int fact(int a){
+        int b = 1;
+        for (int i = 1; i <=a ; i++) {
+            b *= i;
+        }
+        return b;
+    }
 }
--- kr_correct/Main2.java
+++ kr_correct/Main2.java
@@ -1,2 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+4 задание 2 варианта
+ */
 public class Main2 {
+
+    public static void printMatrix(int[][] matrix){
+        for (int[] i:matrix){
+            System.out.println(Arrays.toString(i));
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        double sum = 0;
+        int[][] matrix = new int[k][k];
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        printMatrix(matrix);
+        for (int i = 0; i <k ; i++) {
+            for (int j = 0; j <k ; j++) {
+                if ((i>j) && ((i+j)<(k-1))){
+                    sum+= matrix[i][j];
+                }
+                if ((i<j) && ((i+j)>(k-1))){
+                    sum+= matrix[i][j];
+                }
+                if (i==j) {
+                    sum += matrix[i][j];
+                }
+                if ((i+j)==(k-1)){
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        if (k % 2 ==1) {
+            sum -= matrix[k/2][k/2];
+        }
+        System.out.println(sum);
+    }
 }
