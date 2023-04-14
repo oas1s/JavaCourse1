1a5e9a05f488262ed6629f487bb05cbb93927140
oas1s <azatazat835@mail.ru>
2023-04-14 17:30:09 +0300

    add lesson 16 buffer

--- 16/Main.java
+++ 16/Main.java
@@ -1,25 +1,23 @@
 import java.io.*;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        BufferedOutputStream buff =
-                new BufferedOutputStream(new FileOutputStream("text.txt"),3);
-        buff.write("hiii!".getBytes());
-        buff.close();
 
-        BufferedInputStream in =
-                new BufferedInputStream(new FileInputStream("text.txt"));
-        System.out.println(new String(in.readAllBytes()));
+    }
 
-
-        BufferedWriter bufferedWriter =
-                new BufferedWriter(new FileWriter("text.txt"),3);
-
-        bufferedWriter.write("asd");
-        bufferedWriter.flush();
-        BufferedReader bufferedReader =
-                new BufferedReader(new FileReader("text.txt"));
-
-        System.out.println(bufferedReader.readLine());
+    public static List<String> getFolder(String file){
+        String[] strings = file.split("\\/");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i <strings.length-1 ; i++) {
+            stringBuilder.append(strings[i] + "/");
+        }
+        StringBuilder stringBuilder1 = new StringBuilder();
+        String[] strings1 = strings[strings.length-1].split("\\.");
+        for (int i = 0; i < strings1.length-1; i++) {
+            stringBuilder1.append(strings1[i] + ".");
+        }
+        return List.of(stringBuilder.toString(),stringBuilder1.toString());
     }
 }
