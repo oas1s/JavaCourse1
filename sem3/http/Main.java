c8911668f877dd4330a675692af8e025a031f2ba
oas1s <azatazat835@mail.ru>
2023-09-02 15:21:13 +0300

    update Main

--- sem3/http/Main.java
+++ sem3/http/Main.java
@@ -1,6 +1,7 @@
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception{
@@ -14,9 +15,26 @@ public class Main {
                 "\n").getBytes());
 
         int c;
-
+        StringBuilder stringBuilder = new StringBuilder();
         while ((c =inputStream.read()) != -1){
-            System.out.print((char) c);
+            stringBuilder.append((char) c);
         }
+
+        String s = stringBuilder.toString();
+        String[] strings = s.split("\r?\n");
+
+        StringBuilder stringBuilder1 = new StringBuilder();
+        boolean flag = false;
+        for (int i = 0; i <strings.length ; i++) {
+            if (flag){
+               stringBuilder1.append(strings[i]);
+            } else {
+                if (strings[i].isEmpty()){
+                    flag = true;
+                }
+            }
+        }
+
+        System.out.println(stringBuilder1);
     }
 }
